64bddf219e256fb25909fd5378a107fa5f024aef
Prasant Paudel <prashant.paudel.555@gmail.com>
2022-06-12 17:34:02 +0545

    enable Channel Layer

--- django_channels/settings.py
+++ django_channels/settings.py
@@ -74,6 +74,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_channels.wsgi.application'
 ASGI_APPLICATION = 'django_channels.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379),],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
